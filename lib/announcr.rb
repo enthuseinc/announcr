57242b120cd83e7a91ecb22fcad80835489f9af6
Michael-Keith Bernard <mkbernard@enthu.se>
2013-01-22 15:20:09 -0800

    Top level announcer DSL is now a namespace

--- lib/announcr.rb
+++ lib/announcr.rb
@@ -1,49 +1,32 @@
 require "announcr/version"
 
-require "announcr/prefix"
 require "announcr/namespace"
 require "announcr/event"
 require "announcr/event_scope"
 require "announcr/backends"
 
 module Announcr
-  extend Prefix
+  def self.root_namespace
+    @root_namespace ||= Namespace.new(:root)
+  end
+
+  def self.configure(&block)
+    root_namespace.configure(&block)
+  end
+
+  def self.namespace(name, opts = {}, &block)
+    root_namespace.namespace(name, opts, &block)
+  end
+
+  def self.announce(event_name, opts = {}, &block)
+    root_namespace.announce(event_name, opts, &block)
+  end
+
+  def self.announce_local(event_name, opts = {}, &block)
+    root_namespace.announce_local(event_name, opts, &block)
+  end
 
   def self.reset!
-    @namespaces = {}
-    @backends = {}
-    @global_prefix = nil
+    @root_namespace = nil
   end
-
-  def self.namespace(name, &block)
-    reg_name = (opts.delete(:as) || name).to_s.downcase
-
-    ns = Namespace.new(name)
-    ns.describe(&block) if block_given?
-    @namespaces[reg_name] = ns
-  end
-
-  def self.backend(klass, opts = {})
-    be = klass.new(opts)
-    @backends[be.short_name] = be
-  end
-
-  def self.announce_all(*args)
-    @namespaces.values.each do |ns|
-      ns.announce(*args)
-    end
-  end
-
-  def self.backend_for(name)
-    @backends.fetch(name.to_s.downcase)
-  end
-
-  def self.namespace_for(name)
-    @namespaces.fetch(name.to_s.downcase)
-  end
-
-  def self.namespaces; @namespaces; end
-  def self.backends; @backends; end
-
-  reset!
 end
